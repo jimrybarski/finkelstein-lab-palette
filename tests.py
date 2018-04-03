7cfbe6ec2985626538d0d30227e78d55ffd32f54
Jim Rybarski <jim@rybarski.com>
2018-04-03 14:35:56 -0500

    Add missing __len__ parameter needed by Matplotlib Add tests to check for success of Matplotlib function that calls __len__ implicitly

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import pytest
 from flabpal import blue, yellow, green, red, gray, grey, orange, pink, brown, purple
-from matplotlib.colors import is_color_like
+from matplotlib.colors import is_color_like, to_rgba_array
 
 
 @pytest.mark.parametrize('color', [blue, yellow, green, red, gray, grey, orange, pink, brown, purple])
@@ -14,3 +14,10 @@ def test_repr(color):
     print(r)
     assert r != '(0, 0, 0)'
     assert r != '(0.0, 0.0, 0.0)'
+
+@pytest.mark.parametrize('color', [blue, yellow, green, red, gray, grey, orange, pink, brown, purple])
+def test_cast_to_rgba_array(color):
+    # Matplotlib will run this on our Color objects
+    result = to_rgba_array(color)
+    assert result is not None
+    assert result.shape == (1,4)
